a9fffb1a5c332b4d0a23bb85ce25dae181432a10
yanmiao.wu <yanmiao.wu@qq.com>
2024-07-26 23:08:41 +0800

    string day 13

---
+++ src/mystring/No71.java
@@ -0,0 +1,33 @@
+package mystring;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class No71 {
+    public String simplifyPath(String path) {
+        // 双端队列
+        Deque<String> queue = new LinkedList<>();
+        // 分割字符
+        String[] ss = path.split("/");
+        for (String s : ss) {
+            if (s.equals("") || s.equals(".")) {
+                continue;
+            } else if (s.equals("..")) {
+                queue.pollLast();
+            } else {
+                queue.offer(s);
+            }
+        }
+        // 拼接
+        StringBuilder sb = new StringBuilder("/");
+        while (!queue.isEmpty()) {
+            sb.append(queue.poll());
+            if (!queue.isEmpty()) {
+                sb.append("/");
+            }
+        }
+        // 判空
+        return sb.toString().equals("") ? "/" : sb.toString();
+    }
+
+}
